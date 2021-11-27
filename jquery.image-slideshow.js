62f19c9f968e732c40f1e574ea71af414880684a
Tyler-Coombs <84094311+Tyler-Coombs@users.noreply.github.com>
2021-11-27 19:46:10 -0330

    Coded proper Start/Stop functionality, added CSS.
    Fixed issues with stopping the slideshow.
    Added more CSS options for plugin users.
    Removed Slideshow title and caption elements from index and added them as part of the plugin.

--- jquery.image-slideshow.js
+++ jquery.image-slideshow.js
@@ -1,5 +1,5 @@
 (function($) {
-    $.fn.imageSlideShow = function() {
+    $.fn.imageSlideShow = function(options) {
         //hide image gallery
         $("#gordieGallery").css("display", "none");
 
@@ -12,75 +12,137 @@
         });
 
         let settings = $.extend( {
-            timeout: 3000
-        })
+            title: "Gordie the Golden Retriever",
+            timeout: 3000,
+            fadeOutTime: 1000,
+            fadeInTime: 1000,
+            imageWidth: "700px",
+            imageHeight: "800px",
+            imageBorder: "25px solid #F0D46D",
+            imageBorderRadius: "15px",
+            stopButton: {
+                color: "rgb(35, 139, 171)",
+                background: "#dcdcdc",
+                height: "40px",
+                width: "100px",
+                border: "5px solid rgb(35, 139, 171)"
+            },
+            startButton: {
+                color: "rgb(35, 139, 171)",
+                background: "#dcdcdc",
+                height: "40px",
+                width: "100px",
+                border: "5px solid rgb(35, 139, 171)"
+            }
+        }, options)
 
         let $startButton;
         let $stopButton;
+        let $image;
+        let $title = "<h1>" + settings.title + "</h1>";
+        $("body").append($title);
+        let $slideShow = $('<img id="slide" src="images/treatsplease.jpg" alt="Treats Please">');
+        $("body").append($slideShow);
+        let $caption = "<h2 id='caption'>" + $("img").attr("alt") + "</h2>";
+        $("body").append($caption);
+
         setStartButtonProperties();
         setStopButtonProperties();
+        setImageProperties();
 
         $("#start").click(function () {
-            startSlideshow();
+            let intervalID = startSlideshow();
+            $("#stop").click(function () {
+                clearInterval(intervalID);
+            });
         });
-        $("#stop").click(function () {
-            stopSlideshow();
+
+        $("#start").hover(function() {
+            $(this).css("background-color", settings.startButton.color);
+            $(this).css("color", "#ffffff");
+        }, function() {
+            $(this).css("background-color", settings.startButton.background);
+            $(this).css("color", settings.startButton.color);
+        });
+        $("#stop").hover(function() {
+            $(this).css("background-color", settings.stopButton.color);
+            $(this).css("color", "#ffffff");
+        }, function() {
+            $(this).css("background-color", settings.stopButton.background);
+            $(this).css("color", settings.stopButton.color);
         });
 
         function startSlideshow() {
             let imageCounter = 0;
-            setInterval( () => {
-                $("#slide").fadeOut(1000,
+            return window.setInterval( () => {
+                $("#slide").fadeOut(settings.fadeOutTime,
                     () => {
                         imageCounter = (imageCounter + 1) % imageCache.length;
                         const nextImage = imageCache[imageCounter];
-                        $("#slide").attr("src", nextImage.src).fadeIn(1000);
-                        $("#caption").text(nextImage.title).fadeIn(1000);
+                        $("#slide").attr("src", nextImage.src).fadeIn(settings.fadeInTime);
+                        $("#caption").text(nextImage.title).fadeIn(settings.fadeInTime);
                     });
-                $("#caption").fadeOut(1000);
+                $("#caption").fadeOut(settings.fadeOutTime);
             }, settings.timeout);
-        }
-
-        function stopSlideshow() {
-            clearInterval(startSlideshow());
         }
 
         function setStartButtonProperties() {
             let prop = {
-                "color": "white",
+                "display": "block",
+                "color": settings.startButton.color,
+                "background-color": settings.startButton.background,
                 "cursor": "pointer",
                 "font-size": "20px",
-                "width": "100px",
-                "height": "40px",
-                "position": "relative",
-                "top": "5px",
-                "right": "5px",
-                "border": "0px",
+                "width": settings.startButton.width,
+                "height": settings.startButton.height,
+                "margin-left": "auto",
+                "margin-right": "auto",
+                "border": settings.startButton.border,
+                "border-radius": "12px",
+                "box-shadow": "0 5px #666",
                 "z-index": "1"
             };
 
             $startButton = $('<button id="start" type="button">START</button>');
             $startButton.css(prop);
-            $("h2").after($startButton);
+            $("#caption").after($startButton);
+
         }
+
         function setStopButtonProperties() {
             let prop = {
-                "color": "white",
+                "display": "block",
+                "color": settings.stopButton.color,
+                "background-color": settings.stopButton.background,
                 "cursor": "pointer",
                 "font-size": "20px",
-                "width": "100px",
-                "height": "40px",
-                "position": "relative",
-                "top": "5px",
-                "right": "5px",
-                "border": "0px",
+                "width": settings.stopButton.width,
+                "height": settings.stopButton.height,
+                "margin-left": "auto",
+                "margin-right": "auto",
+                "border": settings.stopButton.border,
+                "border-radius": "12px",
+                "box-shadow": "0 5px #666",
                 "z-index": "1"
             };
 
             $stopButton = $('<button id="stop" type="button">STOP</button>');
             $stopButton.css(prop);
-            $("h2").after($stopButton);
+            $("#start").after($stopButton);
         }
 
+        function setImageProperties() {
+            $image = $("#slide");
+            $image.css( {
+                "display": "block",
+                "width": settings.imageWidth,
+                "height": settings.imageHeight,
+                "border": settings.imageBorder,
+                "border-radius": settings.imageBorderRadius,
+                "margin-left": "auto",
+                "margin-right": "auto",
+                "object-fit": "cover"
+            })
+        }
     }
 }(jQuery));
